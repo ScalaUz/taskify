876ebd0c5fe97962c27c406498ddce69ad8b00a5
OG'ABEK <o.shomurotov@uic.group>
2024-01-13 00:17:18 +0500

    feat: font size removed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 export default defineConfig({
   publicDir: 'public',
   plugins: [scalaJSPlugin()],
+  server: {
+    host: true,
+    port: 4000
+  }
 });
