674304bff0f866fcf512e9980b037108a9cd502a
Og'abek Shomurotov <o.shomurotov@uic.group>
2024-01-12 15:23:51 +0500

    feat: tailwindcss added

--- src/main/scala/taskify/JsApp.scala
+++ src/main/scala/taskify/JsApp.scala
@@ -17,6 +17,7 @@ object JsApp extends App {
         onInput.mapToValue --> nameVar,
       ),
       p(
+        cls("text-3xl font-bold underline"),
         "Hello, ",
         child.text <-- nameVar.signal.map(_.toUpperCase),
       ),
