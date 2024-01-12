cff151ce899dfdb053f271ce5a7555c6bdb68ed0
Prince <Prince777_98@mail.ru>
2024-01-12 14:37:48 +0500

    laminar js added

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,1 @@
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.15.0")
-addSbtPlugin("org.scala-js"   % "sbt-jsdependencies" % "1.0.2")
---
+++ src/main/scala/taskify/JsApp.scala
@@ -0,0 +1,25 @@
+package taskify
+
+import com.raquo.laminar.api.L._
+import org.scalajs.dom
+
+object JsApp extends App {
+  println("-- Scala.js app start --")
+
+  lazy val container = dom.document.getElementById("root")
+  val nameVar = Var(initial = "world")
+
+  lazy val appElement =
+    div(
+      label("Your name: "),
+      input(
+        placeholder := "Enter your name here",
+        onInput.mapToValue --> nameVar,
+      ),
+      p(
+        "Hello, ",
+        child.text <-- nameVar.signal.map(_.toUpperCase),
+      ),
+    )
+  render(container, appElement)
+}
